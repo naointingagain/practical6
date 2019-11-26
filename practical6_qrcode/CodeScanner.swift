f9f0a45cd7e3306c2cbcfca6cdd8019ce984cb95
naomi <nomzlyt@gmail.com>
2019-11-26 17:49:25 +0800

    edited code scanner and first view controller

---
+++ practical6_qrcode/CodeScanner.swift
@@ -0,0 +1,16 @@
+//
+//  CodeScanner.swift
+//  practical6_qrcode
+//
+//  Created by MAD2_P01 on 26/11/19.
+//  Copyright © 2019 Naomi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+//import QRCodeReader
+//import AVFoundation
+
+class CodeScanner: UIViewController {
+    
+}
--- practical6_qrcode/FirstViewController.swift
+++ practical6_qrcode/FirstViewController.swift
@@ -6,14 +6,28 @@
 //  Copyright © 2019 Naomi. All rights reserved.
 //
 
-import UIKit    
-class FirstViewController: UIViewController {
+import UIKit
+import QRCode
 
+class FirstViewController: UIViewController, UITextFieldDelegate{
+
+    @IBOutlet weak var txtValue: UITextField!
+    @IBOutlet weak var imageViewQR: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        txtValue.resignFirstResponder()
+        print(txtValue.text!)
+        var qrcode = QRCode(txtValue.text!)
+        qrcode?.color = CIColor(rgba: "880000")
+        qrcode?.backgroundColor = CIColor(rgba : "ffffff")
+        imageViewQR.image = qrcode?.image
+        return true
+    }
 
 }
 
