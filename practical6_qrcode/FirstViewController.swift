48bd972426e0042c92d1a02a978bc16e99eed1ae
naomi <nomzlyt@gmail.com>
2019-11-26 16:39:26 +0800

    Initial Commit

---
+++ practical6_qrcode/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  practical6_qrcode
+//
+//  Created by MAD2_P01 on 26/11/19.
+//  Copyright © 2019 Naomi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ practical6_qrcode/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  practical6_qrcode
+//
+//  Created by MAD2_P01 on 26/11/19.
+//  Copyright © 2019 Naomi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
